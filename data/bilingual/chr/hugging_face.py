3e3deabc0d61c23340073ef579357c0a7b0c4458
lucdenardi <denardiluc@gmail.com>
2026-01-21 16:19:18 -0700

    starting to add monolingual data

---
+++ data/bilingual/chr/hugging_face.py
@@ -0,0 +1,3 @@
+from datasets import load_dataset
+
+ds = load_dataset("wang4067/cherokee-english-45")
---
+++ data/monolingual/oci/hugging_face.py
@@ -0,0 +1,4 @@
+from datasets import load_dataset
+
+# Load the Occitan split from OSCAR
+dataset = load_dataset("oscar-corpus/OSCAR-2201", "Occitan")
