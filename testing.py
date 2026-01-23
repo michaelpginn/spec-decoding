ab9d0a3ada345ba586dc3963ae204ed484645bca
lucdenardi <denardiluc@gmail.com>
2026-01-23 13:48:01 -0700

    continuing trying to run python path

--- testing.py
+++ testing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-data = list(
-    pd.read_csv("data/monolingual/haw/2grams_haw.txt", sep="\t")
-    .to_dict(orient="dict")
-    .values()
-)
+data = pd.read_csv(
+    "data/monolingual/haw/2grams_haw.txt", sep="\t", usecols=[1]
+).to_dict(orient="records")
+
+print(data)
