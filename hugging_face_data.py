1fad88ed680f3e0cee788657d306593d4b17d7be
lucdenardi <denardiluc@gmail.com>
2026-02-02 14:34:11 -0700

    reworking on getting data

--- hugging_face_data.py
+++ hugging_face_data.py
@@ -1,30 +1,36 @@
 import pandas as pd
 from datasets import load_dataset, concatenate_datasets
+from pandas import DataFrame
+
+
+def load_data(df: DataFrame, lang: str):
+    land_df = [data for data in df if data["Language"] == lang]
+    if len(land_df) == 0:
+        return "Wrong input"
+    elif len(land_df) > 0:
+        hugging = [
+            item["hugging face "]
+            for item in land_df
+            if not pd.isna(item["hugging face "])
+        ]
+        loading_data_1, loading_data_2 = (
+            load_dataset(hugging[0]),
+            load_dataset(hugging[1]),
+        )
+        loading_data = concatenate_datasets([loading_data_1, loading_data_2])
+        return loading_data
+    else:
+        hugging = [item["hugging face "] for item in land_df]
+        return hugging
 
 
 def get_data(lang: str, mono_or_bi: str):
     if mono_or_bi == "mono":
         df = pd.read_csv("reference_table_monolingual.csv")
-        land_df = [data for data in df if data["Language"] == lang]
-        if len(land_df) == 0:
-            return "Wrong input"
-        elif len(land_df) > 0:
-            hugging = [
-                item["hugging face "]
-                for item in land_df
-                if not pd.isna(item["hugging face "])
-            ]
-            loading_data_1, loading_data_2 = (
-                load_dataset(hugging[0]),
-                load_dataset(hugging[1]),
-            )
-            loading_data = concatenate_datasets([loading_data_1, loading_data_2])
-            return loading_data
-        else:
-            hugging = [item["hugging face "] for item in land_df]
-            return hugging
+        return load_data(df, lang)
 
     elif mono_or_bi == "bi":
         df = pd.read_csv("reference_table_bilingual.csv")
+        return load_data(df, lang)
     else:
         return "Wrong input"
