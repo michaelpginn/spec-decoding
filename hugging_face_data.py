863c2949abe0fa1886bdfd3954746de9687e8771
lucdenardi <denardiluc@gmail.com>
2026-02-02 14:32:15 -0700

    reworking on getting data

--- hugging_face_data.py
+++ hugging_face_data.py
@@ -1,8 +1,30 @@
 import pandas as pd
+from datasets import load_dataset, concatenate_datasets
 
 
 def get_data(lang: str, mono_or_bi: str):
     if mono_or_bi == "mono":
-        pd.read_csv("reference_table_monolingual.csv")
+        df = pd.read_csv("reference_table_monolingual.csv")
+        land_df = [data for data in df if data["Language"] == lang]
+        if len(land_df) == 0:
+            return "Wrong input"
+        elif len(land_df) > 0:
+            hugging = [
+                item["hugging face "]
+                for item in land_df
+                if not pd.isna(item["hugging face "])
+            ]
+            loading_data_1, loading_data_2 = (
+                load_dataset(hugging[0]),
+                load_dataset(hugging[1]),
+            )
+            loading_data = concatenate_datasets([loading_data_1, loading_data_2])
+            return loading_data
+        else:
+            hugging = [item["hugging face "] for item in land_df]
+            return hugging
+
+    elif mono_or_bi == "bi":
+        df = pd.read_csv("reference_table_bilingual.csv")
     else:
-        pd.read_csv("reference_table_bilingual.csv")
+        return "Wrong input"
--- testing.py
+++ testing.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv("reference_table_monolingual.csv")
-print(df)
+df = pd.read_csv("reference_table_monolingual.csv").to_dict(orient="records")
+land_df = [data for data in df if data["Language"] == "Hawaiian"]
+hugging = [
+    item["hugging face "] for item in land_df if not pd.isna(item["hugging face "])
+]
+print(hugging)
