d2fb5fbeb64a3232e075bbc377156cef13334afb
lucdenardi <denardiluc@gmail.com>
2026-02-02 19:30:55 -0700

    added chinese

--- hugging_face_data.py
+++ hugging_face_data.py
@@ -3,10 +3,14 @@ from datasets import load_dataset, concatenate_datasets
 from pandas import DataFrame
 
 
-def load_data(df: DataFrame, lang: str):
+def load_data(df: DataFrame, lang: str, type: str):
     land_df = [data for data in df if data["Language"] == lang]
     if len(land_df) == 0:
         return "Wrong input"
+    if lang == "Chinese" and type == "monolingual":
+        hugging = [item["hugging face "] for item in land_df]
+        loading_data = load_dataset(hugging[0], "en-zh")
+        return loading_data
     elif len(land_df) > 0:
         hugging = [
             item["hugging face "]
@@ -21,16 +25,17 @@ def load_data(df: DataFrame, lang: str):
         return loading_data
     else:
         hugging = [item["hugging face "] for item in land_df]
-        return hugging
+        loading_data = load_dataset(hugging[0])
+        return loading_data
 
 
 def get_data(lang: str, mono_or_bi: str):
     if mono_or_bi == "mono":
         df = pd.read_csv("reference_table_monolingual.csv")
-        return load_data(df, lang)
+        return load_data(df, lang, mono_or_bi)
 
     elif mono_or_bi == "bi":
         df = pd.read_csv("reference_table_bilingual.csv")
-        return load_data(df, lang)
+        return load_data(df, lang, mono_or_bi)
     else:
         return "Wrong input"
