fb4a5c1efd6b1d528d04c82877b37c6e519a2d37
lucdenardi <denardiluc@gmail.com>
2026-02-14 13:45:39 -0700

    moved to src/data directory and got rid of testing py files

--- testing.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("reference_table_monolingual.csv").to_dict(orient="records")
-land_df = [data for data in df if data["Language"] == "Hawaiian"]
-hugging = [
-    item["hugging face "] for item in land_df if not pd.isna(item["hugging face "])
-]
-print(hugging)
