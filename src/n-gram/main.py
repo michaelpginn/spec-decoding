60f5be1de0589c091fb06dfe91551b7300dcce2b
lucdenardi <denardiluc@gmail.com>
2026-02-21 16:21:10 -0700

    started making n-gram model

--- src/n-gram/main.py
+++ src/n-gram/main.py
@@ -1,4 +1,28 @@
+import pandas as pd
+
+languages: list = [
+    "Berber",
+    "Cherokee",
+    "Hawaiian",
+    "Igbo",
+    "Lakota",
+    "Muskogee(Creek)",
+    "Nepali",
+    "Occitan",
+    "Occitan",
+    "Ojibwe",
+    "Quechua",
+    "Maya",
+    "Tamazight",
+    "Chinese",
+]
+
+
 def main():
+    df = pd.read_csv("./src/data/reference_table_monolingual.csv").to_dict("records")
+    print(df)
+    # for language in languages:
+
     return 0
 
 
