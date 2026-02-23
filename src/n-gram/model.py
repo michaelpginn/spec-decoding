bcd4ae69a3a3ad79d8e3dfea5ebab13728eb816c
lucdenardi <denardiluc@gmail.com>
2026-02-23 08:38:04 -0700

    got trigram done

--- src/n-gram/model.py
+++ src/n-gram/model.py
@@ -29,3 +29,30 @@ class bigram:
             return max(self.model[word], key=lambda k: self.model[word][k])
         else:
             return "No prediction available"
+
+class trigram():
+    def __init__(self, train_list, test_list) -> None:
+        self.train_text = train_list
+        self.test_text = test_list
+        self.model = defaultdict(lambda: defaultdict(lambda: 0.0))
+
+    def ngram_model(self):
+        for sentence in self.train_text:
+            self.train_token = [token for token in sentence.split() if token != ""]
+
+            self.gram = list(zip(self.train_token, self.train_token[1:], self.train_token[2:]))
+
+            for w1, w2, w3 in self.gram:
+                self.model[(w1, w2)][w3] += 1
+
+        for w1_w2 in self.model:
+            total_count = float(sum(self.model[w1_w2].values()))
+            for w3 in self.model[w1_w2]:
+                self.model[w1_w2][w3] /= total_count
+
+    def predict(self, w1, w2):
+        next_word_probs = self.model[w1, w2]
+        if next_word_probs:
+            return max(self.model[(w1,w2)], key=lambda k: self.model[(w1,w2)][k])
+        else:
+            return "No prediction available"
