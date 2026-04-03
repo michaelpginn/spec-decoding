6ec8a798549b925e4dae4a4d78b68dc6bd898fde
lucdenardi <denardiluc@gmail.com>
2026-04-03 09:47:18 -0600

    working on different ngrams

--- src/config/config.py
+++ src/config/config.py
@@ -10,24 +10,24 @@ class ExperimentConfig:
     target_model: str
     draft_model: str | None
     draft_model_type: Literal["none", "neural", "ngram"]
-   
+
     decoding_mode: Literal["greedy", "top_k", "top_p"]
     gamma: int = 5
     track_iterations: bool = False # If true, will log per-iteration of SD
-    
-    ngram_n: int = 3
-    
+
+    ngram_n: int = 5
+
     use_hf_assisted: bool = False
     hf_schedule: Literal["heuristic", "constant"] | None = None
-    
+
     data_source: str = "tatoeba"
     max_samples: int = 5
     max_new_tokens: int = 512
     device: str = "auto"
 
     def __post_init__(self):
-        if self.draft_model == "None": 
+        if self.draft_model == "None":
             self.draft_model = None
-            
+
         if self.draft_model_type == 'neural':
             assert self.gamma > 0
