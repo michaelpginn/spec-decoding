66f514f6330089df73c50fef5e7e1dd261fd4b09
look-luc <denardiluc@gmail.com>
2026-05-07 15:30:45 -0600

    made class into a single function

--- src/story_gen_prompt.py
+++ src/story_gen_prompt.py
@@ -9,27 +9,20 @@ except LookupError:
     nltk.download('wordnet')
     nltk.download('omw-1.4')
 
-class create_prompt:
-    def __init__(self, num_prompts: int = 10) -> None:
-        self.num_prompts = num_prompts
+def create_prompt(language:str, adj_n:bool=False, num_prompts:int=10):
+    prompts: dict = {}
+    if not adj_n:
+        nouns = list(wn.all_synsets('n'))
+        sample_nouns = [synset.name().split('.')[0] for synset in nouns[:num_prompts]]
 
-    def get_combinations(self):
-        adjs = [s.name().split('.')[0] for s in itertools.islice(wn.all_synsets('a'), self.num_prompts)]
-        nouns = [s.name().split('.')[0] for s in itertools.islice(wn.all_synsets('n'), self.num_prompts)]
+        for i in range(num_prompts):
+            prompts[i+1] = f"Write a story in {language} about a(n) {sample_nouns[i]}"
+    else:
+        adjs = [s.name().split('.')[0] for s in itertools.islice(wn.all_synsets('a'), num_prompts)]
+        nouns = [s.name().split('.')[0] for s in itertools.islice(wn.all_synsets('n'), num_prompts)]
+        wombos = [f"{a} {n}" for a, n in zip(adjs, nouns)]
 
-        return [f"{a} {n}" for a, n in zip(adjs, nouns)]
+        for i in range(num_prompts):
+            prompts[i+1] = f"Write a stroy in {language} about a(n) {wombos[i]}"
 
-    def make_prompts(self, language, adj_n:bool=False):
-        self.language = language
-        self.prompts: dict = {}
-        if not adj_n:
-            nouns = list(wn.all_synsets('n'))
-            sample_nouns = [synset.name().split('.')[0] for synset in nouns[:self.num_prompts]]
-
-            for i in range(self.num_prompts):
-                self.prompts[i+1] = f"Write a story in {self.language} about a(n) {sample_nouns[i]}"
-        else:
-            wombos = self.get_combinations() #wombo = word combos
-            for i in range(self.num_prompts):
-                self.prompts[i+1] = f"Write a stroy in {self.language} about a(n) {wombos[i]}"
-        return self.prompts
+    return prompts
