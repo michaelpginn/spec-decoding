1a97f91fb040c22208bf7d26e94d68c443850610
Michael Ginn <michaelginn00@gmail.com>
2026-08-01 18:29:45 +0200

    batched

--- src/measure_divergence.py
+++ src/measure_divergence.py
@@ -6,6 +6,7 @@ import argparse
 from typing import Mapping
 
 import torch
+from torch.utils.data.dataloader import DataLoader
 from tqdm import tqdm
 
 from src.data.dataset import assemble_dataset, get_language_name
@@ -23,14 +24,20 @@ device = next(p_model.parameters()).device
 
 language = get_language_name(args.language_code)
 dataset = assemble_dataset(args.language_code, 'mono', p_tokenizer, None)['test']
+dataloader = DataLoader(
+    dataset,  # type: ignore[arg-type]
+    batch_size=4,
+    shuffle=False,
+    pin_memory=(device.type == "cuda"),
+)
 
 p_model.eval()
 q_model.eval()
 
 mean_kl = 0.
 mean_lk = 0.
-for row in tqdm(dataset):
-    assert isinstance(row, Mapping)
+for batch in tqdm(dataloader):
+    breakpoint()
     inputs = p_tokenizer(row['text'], return_tensors="pt", truncation=True, max_length=128).to(device)
     with torch.no_grad():
         p_out = p_model(**inputs)
